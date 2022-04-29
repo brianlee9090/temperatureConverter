d91488d29f8932f4811076b69eb4b688f82b66ff
Brian Lee <ilovenocchi@gmail.com>
2022-04-29 13:16:28 +0900

    add: readme vid and screenshots

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -7,7 +7,6 @@
       <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
       <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"><p>☽</p></div>
     </div>
-    
   </label>
 </div>
 </template>
